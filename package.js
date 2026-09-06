325cb43ae9e51f8de5eee4513752d9db47eb732c
dhop7528-beep <dhop7528@gmail.com>
2026-09-07 00:22:08 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,25 @@
+{
+"name": "tai-xiu-prediction-api",
+"version": "1.0.0",
+"description": "API dự đoán Tài Xỉu MD5 dựa trên thuật toán SMART và lịch sử phiên",
+"main": "server.js",
+"scripts": {
+"start": "node server.js",
+"dev": "nodemon server.js"
+},
+"keywords": [
+"tai-xiu",
+"predict-api",
+"smart-algorithm"
+],
+"author": "AI Assistant",
+"license": "ISC",
+"dependencies": {
+"axios": "^1.6.8",
+"cors": "^2.8.5",
+"express": "^4.19.2"
+},
+"devDependencies": {
+"nodemon": "^3.1.0"
+}
+}
